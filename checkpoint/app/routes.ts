797681e527fc9904589c576e82d4b4c269d5e4c7
andres <andresriosaragoncode@gmail.com>
2025-06-15 19:32:52 +1200

    created basic react vite app

---
+++ checkpoint/app/routes.ts
@@ -0,0 +1,9 @@
+
+
+import {
+  type RouteConfig,
+  route,
+} from "@react-router/dev/routes";
+import { flatRoutes } from "@react-router/fs-routes";
+
+export default flatRoutes() satisfies RouteConfig;
